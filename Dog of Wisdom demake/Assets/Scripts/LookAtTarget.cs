5aa8754847754223c0eb417b113ee44d24dc22d6
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-04-23 12:08:54 +0200

    Add Dog of Wisdom

---
+++ Dog of Wisdom demake/Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    private void Update()
+    {
+        transform.LookAt(_target);
+    }
+}
